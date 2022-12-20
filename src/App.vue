5129f0f29f009732d806ecefb09dcb22dc7062d4
Paulo Pariz <paulopariz@MacBook-Pro-de-Paulo.local>
2022-12-20 18:51:54 -0300

    aula5/ v-for

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,6 @@
-<template>
+<!-- ESTE É O TEMPLETE REFERENTE Á AULA 4 | v-if & v-show -->
+
+<!-- <template>
     <TheHeader
     v-if="showHeader"
     ></TheHeader>
@@ -21,7 +23,7 @@
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
-import TheHeader from './components/TheHeader.vue'
+import TheHeader from './components/aula4/TheHeader.vue'
 
 export default {
   name: 'App',
@@ -52,4 +54,92 @@ data() {
   color: #2c3e50;
   margin-top: 60px;
 }
+</style> -->
+
+<!-- FIM TEMPLETE REFERENTE Á AULA 4 | v-if & v-show -->
+
+
+
+<!--///////////////////////////////////////-->
+
+
+
+<!-- ESTE É O TEMPLETE REFERENTE Á AULA 5 | v-for -->
+
+<template>
+
+  <div>
+
+    <div v-for="(obj, index) in todos" v-bind:key="obj.id">
+      <ul>
+        <li>{{ index }} - {{ obj.title }}</li>
+      </ul>
+    </div>
+
+  </div>
+
+</template>
+
+<script>
+  export default {
+    name: 'App',
+    components: {
+
+    },
+
+    data() {
+      return {
+        todos: [{
+            "userId": 1,
+            "id": 1,
+            "title": "delectus aut autem",
+            "completed": false
+          },
+          {
+            "userId": 1,
+            "id": 2,
+            "title": "quis ut nam facilis et officia qui",
+            "completed": false
+          },
+          {
+            "userId": 1,
+            "id": 3,
+            "title": "fugiat veniam minus",
+            "completed": false
+          },
+          {
+            "userId": 1,
+            "id": 4,
+            "title": "et porro tempora",
+            "completed": true
+          },
+          {
+            "userId": 1,
+            "id": 5,
+            "title": "laboriosam mollitia et enim quasi adipisci quia provident illum",
+            "completed": false
+          }
+        ]
+      }
+    }
+
+  }
+</script>
+
+<style>
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+
+  li {
+    list-style: none;
+    color: black;
+    font-weight: 700;
+  }
 </style>
+
+<!-- FIM TEMPLETE REFERENTE Á AULA 5 | v-for -->
