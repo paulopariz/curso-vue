1db9ac374aff295f6ded10a292d904863bcc580c
Paulo Pariz <paulopariz@MacBook-Pro-de-Paulo.local>
2022-12-20 18:00:54 -0300

    aula4/ v-if & v-show

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,45 @@
 <template>
+    <TheHeader
+    v-if="showHeader"
+    ></TheHeader>
+
+  <div v-show="showNme">
+    nome: {{ name }} <br> 
+    sobrenome: {{ sobreNome }}
+  </div>
+
+
+  <div v-if="accessLevel === 'admin'">Admin</div>
+  <div v-else-if="accessLevel === 'marketing'">Marketing</div>
+  <div v-else>User</div>
+
+
+
   <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js App"/>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import TheHeader from './components/TheHeader.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    TheHeader
+  },
+
+data() {
+  return {
+    showHeader: true,
+    name: 'PAULO',
+    sobreNome: 'PARIZ',
+    showNme: false,
+    accessLevel: 'marketing'
   }
+}
+
 }
 </script>
 
---
+++ src/components/TheHeader.vue
@@ -0,0 +1,16 @@
+<template>
+    <header class="header">header</header>        
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style>
+    .header{
+        background-color: black;
+        color: white;
+    }
+</style>
